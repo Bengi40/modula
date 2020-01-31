43dc619557ee62644631a917a91e69dacabaa292
bengi40 <gonzalez.benjamin47@gmail.com>
2020-01-31 22:10:55 +0100

    Init project

---
+++ admin/index.php
@@ -0,0 +1,22 @@
+<?php
+	require_once('../config.php');
+	require_once('../functions.php');
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title></title>
+
+    <link rel="stylesheet" href="includes/styles/style.css">
+
+</head>
+<body>
+	<div class="wrapper">
+
+	</div>
+</body>
+
+	<script src="includes/scripts/js/jquery.js"></script>
+    <script src="includes/scripts/js/script.js"></script>
+</html>
