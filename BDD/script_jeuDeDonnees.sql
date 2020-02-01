47f6e433868d1419bd16cf1be9b0d85bd7e61a31
bengi40 <gonzalez.benjamin47@gmail.com>
2020-02-01 11:21:30 +0100

    script bdd + jeu de donnée

---
+++ BDD/script_jeuDeDonnees.sql
@@ -0,0 +1,11 @@
+USE modual;
+INSERT INTO contacts
+VALUES 
+(1,"Gonzalez","Benjamin","gonzalez.benjamin47@gmail.com","Bonjour, votre produit est vraiment super !",true,'2020-01-31 21:39:33',"192.168.1.251"),
+(2,"Jean","Claude","jc@gmail.com","Bonjour, j'adore ce que vous faites !!",true,'2020-01-31 21:39:37',"65.18.1.21"),
+(3,"Bond","James","bj@gmail.com","So cute !",true,'2020-01-31 22:45:33',"65.18.1.21"),
+(4,"Renault","Mégane","rm@gmail.com","Bonjour, j'adore ce que vous faites !!",true,'2020-02-01 10:25:33',"65.18.1.21"),
+(5,"Jean","Claude","jc@gmail.com","Bonjour, j'adore ce que vous faites !!",true,'2020-02-01 21:39:33',"65.18.1.21");
+
+INSERT INTO login
+VALUES(1,"admin","admin");
