2102f8d0efd417300334483956233da2d0195977
bengi40 <gonzalez.benjamin47@gmail.com>
2020-02-03 11:43:09 +0100

    Intégration ip client

--- functions.php
+++ functions.php
@@ -12,14 +12,13 @@ function isEmail($email) {
 }
 
 function getIp(){
-	$ip = $_SERVER['REMOTE_ADDR'];
-//     // if(!empty($_SERVER['HTTP_CLIENT_IP'])){
-//     //   $ip = $_SERVER['HTTP_CLIENT_IP'];
-//     // }elseif(!empty($_SERVER['HTTP_X_FORWARDED_FOR'])){
-//     //   $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
-//     // }else{
-//       $ip = $_SERVER['REMOTE_ADDR'];
-//     //}
+    if(!empty($_SERVER['HTTP_CLIENT_IP'])){
+      $ip = $_SERVER['HTTP_CLIENT_IP'];
+    }elseif(!empty($_SERVER['HTTP_X_FORWARDED_FOR'])){
+      $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
+    }else{
+     $ip = $_SERVER['REMOTE_ADDR'];
+    }
      return $ip;
 }
 
