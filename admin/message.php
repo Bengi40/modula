8dd7585c4a52dec1e7a632f8fa176c8e802f33d7
bengi40 <gonzalez.benjamin47@gmail.com>
2020-02-05 16:56:17 +0100

    session ouverte au démarrage corriger

--- admin/message.php
+++ admin/message.php
@@ -3,8 +3,6 @@ session_start();
 require_once('../config.php');
 require_once('../functions.php');
 
-$_SESSION['role'] = "admin";
-
 $db = Database::connect();
 $statement = $db->query('
 			SELECT 	id,date,heure,email
